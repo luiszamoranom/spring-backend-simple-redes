00d96cfbad92612b67362e2fb77fb2f5ba32ef90
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2023-12-06 00:55:17 -0300

    feat(*): se crea el proyecto

---
+++ src/main/java/com/luiszamorano/backendsimpleredes/BackendSimpleRedesApplication.java
@@ -0,0 +1,13 @@
+package com.luiszamorano.backendsimpleredes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSimpleRedesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendSimpleRedesApplication.class, args);
+	}
+
+}
